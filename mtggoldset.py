705f7993557ef568309f5a606b40bbfce12f0bc5
Stephen <gimpelstephen1@gmail.com>
2016-08-18 22:58:37 -0700

    Algorithm clean up

--- mtggoldset.py
+++ mtggoldset.py
@@ -5,14 +5,26 @@ import requests
 import urllib
 import random
 from math import floor
+from multiprocessing.pool import ThreadPool
+#Below is a suggested solution for memory issues
+#def create_packs(n,lib):
+ # while n:
+   # n-=1
+   # l = []
+  #  l.extend(random.choice(lib["common"]) for i in range(10))
+  #  l.extend(random.choice(lib["whatever"]) for i in range(3))
+  #  if(random.random() < 0.125):
+	#	pack[count] = mythics[floor(random.random() * len(mythics))]
+#	else:
+#	pack[count] = rares[floor(random.random() * len(rares))]
+#yield l
+
 def getRarity(rare, cards):
 	cardlist = {}
 	count = 0
 	for ci in cards:
-		#print(cards[ci]["rarity"])
 		if(cards[ci]["rarity"] == rare):
 			cardlist[count] = cards[ci]
-			#print(cardlist[count])
 			count += 1
 	
 	return cardlist
@@ -62,41 +74,52 @@ def getset(name):
 		count +=1
 	return cardlist
 
-def average(cardlist):
+#function used to find average pack value
+def average(numlist):
 	count = 0
 	value = 0
-	while count < len(cardlist):
-		value += cardlist[count]
+	for i in range(len(numlist)):
+		value += numlist[count]
 		count+=1
-	return value/len(cardlist)
-#main();
+	return value/len(numlist)
+
+#function used to find average box value, not used at this time due to runtime inefficency
+def averageBox(numlist, totalBoxes, boxSize):
+	count = 0
+	value = 0
+	for i in range(totalBoxes * boxSize):
+		value += numlist[count]
+		count += 1
+	return value/totalBoxes
+
+def boxCreation(boxCount, packCount):
+	value = {}
+	count = 0
+	rare_values = {}
+	for i in range(boxCount * packCount):
+		pack = makePack(commons, uncommons, rares, mythics)
+		rare_values[count] = pack[13]["value"]
+		value[count] = getPackValue(pack)
+		count+=1
+	return value, rare_values
 #start of program
-#transfering
 setName = raw_input("Please input the acronym for the set (I.E Khans of Tarkir is KTK): ")
-clist = getset(setName)
-if(clist == {}):
+try:
+	clist = getset(setName)
+except:
 	print("An error occured when getting the set, please check your acronym or try again later.")
 	exit()
+
+number_of_boxes = 1000
 commons = getRarity("Common", clist)
 uncommons = getRarity("Uncommon", clist)
 rares = getRarity("Rare", clist)
 mythics = getRarity("Mythic", clist)
-value = {}
-scount = 0
-pcount = 0
-vcount = 0
-rare_values = {}
-pack_count = eval(raw_input("How many packs are in this box: "))
-while(scount < 10000):
-	while(pcount < pack_count):
-		pack = makePack(commons, uncommons, rares, mythics)
-		rare_values[vcount] = pack[13]["value"]
-		value[vcount] = getPackValue(pack)
-		pcount += 1
-		vcount +=1
-	scount+=1
-	pcount=0
-print(value)
+
+pack_count = eval(raw_input("How many packs do you want to open: "))
+value, rare_values = boxCreation(number_of_boxes, pack_count)
 print("Set Name:{}".format(setName))
-print("Total Average Box Value: {}".format(average(value)))
-print("Total Average Value of Rares in a Box: {}".format(average(rare_values)))
+print("Total Average Pack Value: {}".format(average(value)))
+print("Total Average Value of Rares/Mythics in a Pack: {}".format(average(rare_values)))
+print("Total Average Box Value: {}".format(averageBox(value, number_of_boxes, pack_count)))
+print("Total Average Value of Rares/Mythics in a Box: {}".format(averageBox(rare_values, number_of_boxes, pack_count)))
